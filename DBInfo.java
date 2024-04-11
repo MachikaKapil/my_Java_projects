bf935765216274baadb9a4abf9ab770c6db6dfe3
MachikaKapil <162136385+MachikaKapil@users.noreply.github.com>
2024-04-11 15:59:06 +0530

    Add files via upload
    servlet project

---
+++ DBInfo.java
@@ -0,0 +1,7 @@
+package test;
+public interface DBInfo 
+{
+   public static final String dburl = "jdbc:oracle:thin:@localhost:1521:xe";
+   public static final String uName = "system";
+   public static final String pWord = "Kapil123143";
+}
